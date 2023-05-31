1c8878e53107464548d880a058a16d98c358cf70
Thailson Bezerra <thailson.lima@beyondtecnologia.com.br>
2023-05-31 15:56:23 -0300

    style: adicionado estilização no header

--- src/Components/Header/Header.module.css.d.ts
+++ src/Components/Header/Header.module.css.d.ts
@@ -3,6 +3,7 @@ declare module "*.module.css";
 declare const styles: {
     readonly header: string 
     readonly link: string 
+    readonly header_bg:string
 
 };
 export default styles;
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,14 +1,21 @@
 import { NavLink } from "react-router-dom";
+import foto from '../../img/rick.png';
+
 import styles from "./Header.module.css"
 
 const Header = () => {
     return (
-        <nav className={styles.header}>
-            <ul>
-                <li><NavLink className={styles.link} to="/" end>Personagens</NavLink></li>
-                <li><NavLink className={styles.link} to="sobre">Sobre</NavLink></li>                
-            </ul>
-        </nav>
+        <header className={styles.header_bg}>
+            <div className={styles.header}>
+                    <a><img src={foto} alt="icone rick" /></a>
+                    <nav>
+                        <ul>
+                            <li><NavLink className={styles.link} to="/" end>Personagens</NavLink></li>
+                            <li><NavLink className={styles.link} to="sobre">Sobre</NavLink></li>   
+                        </ul>             
+                    </nav>
+            </div>
+        </header>
     );
 }
  
