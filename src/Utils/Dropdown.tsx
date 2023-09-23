3eb1b298c4fe81c34d67e15d01037b7ce52e73ab
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-09-23 19:19:59 -0300

    feat: component e css dropdown

---
+++ src/Utils/Dropdown.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./Dropdown.css";
+
+interface DropdownProps {
+  children: React.ReactNode;
+  items: string[];
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ children, items }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [scrollStyle, setScrollStyle] = useState("");
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+    setScrollStyle(isOpen ? "" : "scroll");
+  };
+
+  return (
+    <>
+      <button onClick={toggleDropdown} className="dropdown-button">
+        {children}
+      </button>
+
+      <div className={`dropdown ${scrollStyle}`}>
+        {isOpen && (
+          <ul className="dropdown-menu">
+            {items.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Dropdown;
