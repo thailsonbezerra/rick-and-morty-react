79ccd7857381b103744305c56350c0510ff168f9
Thailson Bezerra <thailsonbezerra@outlook.com>
2024-10-17 22:01:18 -0300

    backup

--- src/Components/TypeIt/TypeIt.module.css.d.ts
+++ src/Components/TypeIt/TypeIt.module.css.d.ts
@@ -7,6 +7,10 @@ declare const styles: {
   readonly rest: string;
   readonly input: string;
   readonly character_name: string;
+  readonly score: string;
+  readonly score_sequence_success: string;
+  readonly score_input_success: string;
+  readonly score_input_fail: string;
 };
 
 export default styles;
